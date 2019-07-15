955b1ff855468ab8844426d47534612879cfa5b1
Kevin Sung <kevin.sung.123@gmail.com>
2019-07-15 12:40:03 -0400

    Updates

---
+++ precache-manifest.b799aa647833a871e85af25b470478ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ee0a2681c176d93531b1a098a7455db",
+    "url": "/solarpixsandbox/static/media/categoryListFile_salessv.0ee0a268.bin"
+  },
+  {
+    "revision": "1ad945962141fc1462abe9ebfc1e5857",
+    "url": "/solarpixsandbox/static/media/categoryListFile_pcsv.1ad94596.bin"
+  },
+  {
+    "revision": "d1a472a9c02578281e51ef9fca943308",
+    "url": "/solarpixsandbox/static/media/categoryListFile_install.d1a472a9.bin"
+  },
+  {
+    "revision": "9133a88d41cfed73b6d8",
+    "url": "/solarpixsandbox/static/js/runtime~main.c21bbe37.js"
+  },
+  {
+    "revision": "2e4956535c52b1738ac8",
+    "url": "/solarpixsandbox/static/js/main.e27aef8c.chunk.js"
+  },
+  {
+    "revision": "e057b24a57762bd4482b",
+    "url": "/solarpixsandbox/static/js/2.c54d6575.chunk.js"
+  },
+  {
+    "revision": "563260dba5137e24f407fe9a10191819",
+    "url": "/solarpixsandbox/index.html"
+  }
+];
