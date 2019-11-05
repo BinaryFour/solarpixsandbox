978e308b7e63acb78332e138efc87aff9d3f4dc8
Kevin Sung <kevin.sung.123@gmail.com>
2019-11-05 15:01:45 -0500

    Updates

---
+++ precache-manifest.9c9df4a290ce72a0d5fbc17168b1686e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876a80894ea359f5a8f1374f5d8a4640",
+    "url": "/solarpixsandbox/index.html"
+  },
+  {
+    "revision": "952dc50df34a5e00cd68",
+    "url": "/solarpixsandbox/static/css/2.f0405d8a.chunk.css"
+  },
+  {
+    "revision": "952dc50df34a5e00cd68",
+    "url": "/solarpixsandbox/static/js/2.c1b5e815.chunk.js"
+  },
+  {
+    "revision": "9ca41951f8dae27845bd",
+    "url": "/solarpixsandbox/static/js/main.1f1b20f6.chunk.js"
+  },
+  {
+    "revision": "d6108bea07734f2518c9",
+    "url": "/solarpixsandbox/static/js/runtime-main.0efaf03f.js"
+  },
+  {
+    "revision": "c5af74654b1b7de46ae2f732be5d0256",
+    "url": "/solarpixsandbox/static/media/categoryListFile_install.c5af7465.bin"
+  },
+  {
+    "revision": "1ad945962141fc1462abe9ebfc1e5857",
+    "url": "/solarpixsandbox/static/media/categoryListFile_pcsv.1ad94596.bin"
+  },
+  {
+    "revision": "f15da29961ca85776eb5fe5c3272624d",
+    "url": "/solarpixsandbox/static/media/categoryListFile_salessv.f15da299.bin"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/solarpixsandbox/precache-manifest.3546a51a9b46d33208aa4c77f30cc3d9.js"
+  "/solarpixsandbox/precache-manifest.9c9df4a290ce72a0d5fbc17168b1686e.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/solarpixsandbox/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/solarpixsandbox/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
